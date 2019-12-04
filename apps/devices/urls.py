4d459ab1a699b9ab83a3cead4335f10ee08e78f8
cmx_company <569578851@qq.com>
2019-12-04 18:41:12 +0800

    展示所有设备

--- apps/devices/urls.py
+++ apps/devices/urls.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = "dzt"
 __date__ = "2019/9/20"
+
+from django.conf.urls import url
+from .views import DevicesInfoView
+
+
+urlpatterns = [
+    url(r'^info/(?P<station_id>\d+)/$', DevicesInfoView.as_view(), name='station_devices'),
+
+]
--- apps/devices/views.py
+++ apps/devices/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect, JsonResponse
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .models import DevicesInfo, StationInfo
+from myutils.mixin_utils import LoginRequiredMixin
+from myutils.utils import create_history_record
+
+
+class DevicesInfoView(LoginRequiredMixin, View):
+    def get(self, request, station_id):
+        permission = request.user.permission
+        print(permission)
+        if permission == 'superadmin':
+            all_devices = DevicesInfo.objects.all()
+        else:
+            try:
+                company = request.user.company.company_name
+                # print(company)
+            except Exception as e:
+                print(e)
+                return HttpResponseRedirect(reverse('devices_info'))
+            if company:
+                all_devices = DevicesInfo.objects.filter(station_id=station_id)
+            else:
+                all_devices = ""
+        try:
+            station_name = StationInfo.objects.get(id=station_id).station_name
+        except StationInfo.DoesNotExist:
+            create_history_record(request.user, '没有这个测站点')
+
+        create_history_record(request.user, '查询测站点%s所有设备' % station_name)
+        return render(request, 'station_devices.html', {
+            "all_devices": all_devices,
+            "station_id": station_id
+        })
